5ea07ece820e34f4405a08a7885cd6d2910a7c48
Marco Sportillo <msportillo@gmail.com>
2020-05-04 09:24:59 +0200

    Lesson 3 Quiz 3
    Lesson 3 Quiz 3

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,17 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+/*
+
+    ---     Lesson 3 - Quiz 3   ---
+
+
+ var awesomeThoughts = "This is awesome!";
+ var funThoughts = awesomeThoughts.replace("awesome","fun");
+
+console.log(awesomeThoughts);
+console.log(funThoughts);
+$( "#main" ).append(funThoughts);
+
+*/
